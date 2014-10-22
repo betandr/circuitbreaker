8d8b84d57ac2bff2ed0c36e3686884b7e6ffb38c
Elisabeth Anderson <beth@vintur.cc>
2014-10-22 13:36:38 +0100

    Added class impl and composer.json

---
+++ src/CircuitBreaker/CircuitBreaker.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Circuit Breaker implementation
+ *
+ * PHP version 5.4
+ *
+ * @package  CircuitBreaker
+ * @author   Elisabeth Anderson <bet@andr.io>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     https://github.com/bbc-rmp/deploy-pipeline
+ */
+
+namespace betandr\CircuitBreaker;
+
+/**
+ * Circuit Breaker
+ *
+ * PHP version 5.4
+ *
+ * @package  PackageName
+ * @author   Elisabeth Anderson <bet@andr.io>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     https://github.com/bbc-rmp/deploy-pipeline
+ */
+class CircuitBreaker
+{
+    /**
+     * Check if circuit breaker is currently closed (an antonym for isOpen())
+     *
+     * @return a boolean
+     */
+    public function isClosed()
+    {
+        return !$this->isOpen();
+    }
+
+    /**
+     * Check if circuit breaker is currently open
+     *
+     * @return a boolean
+     */
+    public function isOpen()
+    {
+        return true;
+    }
+
+    /**
+     * Log a successful transaction
+     */
+    public function success()
+    {
+
+    }
+
+    /**
+     * Log an unsuccessful transaction
+     */
+    public function failure()
+    {
+
+    }
+
+    /**
+     * To string
+     *
+     * @return a string value representing the object
+     */
+    public function to_s()
+    {
+        return ($this->isClosed()) ? "CircuitBreaker [CLOSED]" : "CircuitBreaker [OPEN]";
+    }
+}
